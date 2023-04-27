b6154ab5cb5cf01f85b7bb66c287f118b91d4be8
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-04-27 16:12:36 -0700

    added april 27 assignment server

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const server = express();
+
+server.get('/heartbeat',(req, res) => {
+    res.json({
+        "is":"working",
+    });
+});
+
+server.listen(8080, () => {
+    console.log("This server is running at PORT 8080")
+});
