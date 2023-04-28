a14e38711ce5915c8f5c04f6c8c3ee470699a103
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-04-27 17:45:36 -0700

    connected database

--- server.js
+++ server.js
@@ -1,5 +1,37 @@
 const express = require("express");
+const pgp = require('pg-promise')();
+
 const server = express();
+
+const cn = {
+    host: 'localhost',
+    port: 5432,
+    database: 'sept2022',
+    user: "christinabarron",
+    password: ' ',
+    allowExitOnIdle: true
+};
+
+const db = pgp(cn);
+
+server.get('/students', async (req, res) => {
+    try {
+        const students = await getStudents();
+        res.json({
+            students
+        })
+    }
+    catch(error) {
+        res.json({
+            error
+        })
+    }
+});
+
+async function getStudents() {
+    const students = await db.any('select * from students', [true]);
+    return students
+}
 
 server.get('/heartbeat',(req, res) => {
     res.json({
